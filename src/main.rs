e1fd60f01ed5099500f2c3d5563088795de728cf
Tomaž Hribernik <tomaz.hribernik@protonmail.com>
2024-01-15 17:43:58 +0100

    Version 1.1.0
    Display parallel outputs in a stream like fashion.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,49 @@
 use std::io::{self, BufRead};
+use std::process::{Command, Output, ExitStatus};
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
-use std::process::Command;
-use std::sync::{Arc, Mutex};
+
+enum ThreadEvent {
+    Success(Output),
+    Fail(Output),
+    Finished,
+}
+
+fn execute_command(cmd: String, tx: Sender<ThreadEvent>) {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", &cmd])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("bash")
+            .arg("-c")
+            .arg(&cmd)
+            .output()
+            .expect("failed to execute process")
+    };
+
+    if ExitStatus::success(&output.status) {
+        tx.send(ThreadEvent::Success(output)).unwrap();
+    } else {
+        tx.send(ThreadEvent::Fail(output)).unwrap();
+    }
+}
+
+fn print_outputs(rx: Receiver<ThreadEvent>) {
+    loop {
+        let event = rx.recv().unwrap();
+        match event {
+            ThreadEvent::Success(output) | ThreadEvent::Fail(output) => {
+                let output = String::from_utf8_lossy(&output.stdout);
+                print!("{}", output);
+            },
+            ThreadEvent::Finished => {
+                return;
+            }
+        }
+    }
+}
 
 fn main() {
     let stdin = io::stdin();
@@ -9,48 +51,31 @@ fn main() {
 
     // Create thread handle vector
     let mut handles = Vec::new();
-    // Create command output vector
-    let outputs = Arc::new(Mutex::new(Vec::new()));
+
+    // Channel used to exchange parallel outputs
+    let (tx, rx) = channel();
 
     // Spawn each command in its own thread
-    for (idx, line) in stdin.lock().lines().enumerate() {
-        let outputs = outputs.clone();
+    for line in stdin.lock().lines() {
         // Replace {} in each line with given command
         let line = line.unwrap();
         let cmd = cmd.replace("{}", &line);
+        let tx = tx.clone();
 
-        let handler = thread::spawn(move || {
-            let output = if cfg!(target_os = "windows") {
-                Command::new("cmd")
-                    .args(["/C", &cmd])
-                    .output()
-                    .expect("failed to execute process")
-            } else {
-                Command::new("bash")
-                    .arg("-c")
-                    .arg(&cmd)
-                    .output()
-                    .expect("failed to execute process")
-            };
-
-            // Push command output to thread
-            outputs.lock().unwrap().push((idx, output));
-        });
+        // Launch each command in its own thread
+        let handler = thread::spawn(move || execute_command(cmd, tx));
         handles.push(handler);
     }
+
+    // Display output
+    let display_thread = thread::spawn(move || print_outputs(rx));
 
     // Wait for all threads to finish
     for handle in handles {
         handle.join().unwrap();
     }
 
-    // Sort output using indices
-    let mut outputs = outputs.lock().unwrap();
-    outputs.sort_by(|a, b| a.0.cmp(&b.0));
-
-    // Display output
-    for output in outputs.iter() {
-        let output = String::from_utf8_lossy(&output.1.stdout);
-        print!("{}", output);
-    }
+    // Wait for display thread
+    tx.send(ThreadEvent::Finished).unwrap();
+    display_thread.join().unwrap();
 }
